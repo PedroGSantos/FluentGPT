f59db61d4034a2528f6bc4c03d1af5b155471291
PedroGSantos <pedrogsantos_10@hotmail.com>
2023-06-27 13:53:18 -0300

    feat: creating navigation tabs and using fragments

---
+++ app/src/main/java/com/example/fluentgpt/Conversations.kt
@@ -0,0 +1,49 @@
+package com.example.fluentgpt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fluentgpt.adapter.ItemAdapter
+import com.example.fluentgpt.data.Datasource
+import com.example.fluentgpt.databinding.ActivityMainBinding
+import com.example.fluentgpt.databinding.FragmentConversationsBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Conversations.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Conversations : Fragment() {
+    private lateinit var binding: FragmentConversationsBinding;
+    private lateinit var itemAdapter: ItemAdapter;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TODO: BINDING
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentConversationsBinding.inflate(layoutInflater, container, false);
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myDataset = Datasource().loadConversations()
+        itemAdapter = ItemAdapter(requireContext(), myDataset)
+        binding.listConversations.adapter = itemAdapter
+        binding.listConversations.setHasFixedSize(true)
+    }
+
+}
--- app/src/main/java/com/example/fluentgpt/MainActivity.kt
+++ app/src/main/java/com/example/fluentgpt/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.fluentgpt.adapter.ItemAdapter
 import com.example.fluentgpt.data.Datasource
 import com.example.fluentgpt.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,10 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val firstFragment=Conversations()
+        val secondFragment=Routine()
+        val thirdFragment=Settings()
 
-        val myDataset = Datasource().loadConversations();
-        binding.listConversations.adapter = ItemAdapter(this, myDataset);
-        binding.listConversations.setHasFixedSize(true)
+        setCurrentFragment(firstFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.conversations->setCurrentFragment(firstFragment)
+                R.id.timeline->setCurrentFragment(secondFragment)
+                R.id.settings->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
 
     }
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
